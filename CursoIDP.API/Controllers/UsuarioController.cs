921112a3b1eff2b027029fd469bfabfe596a8e28
Alberto Picazo <apicazo@plainconcepts.com>
2020-10-29 15:02:06 +0100

    Agregar archivos de proyecto.

---
+++ CursoIDP.API/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoIDP.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoIDP.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Usuario> Get()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            usuarios.Add(new Usuario
+            {
+                Email = "usuario@gmail.com",
+                Nombre = "Alejandro",
+                Telefono = "111111111"
+            });
+
+            return usuarios;
+        }
+    }
+}
---
+++ CursoIDP.API/ViewModels/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoIDP.API.ViewModels
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
